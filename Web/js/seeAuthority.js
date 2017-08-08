06d8c03561a628c5e27c09576855786084f64091
changchangge <1173484831@qq.com>
2017-08-09 00:25:55 +0800

    修改了字符串比较问题

--- Web/js/seeAuthority.js
+++ Web/js/seeAuthority.js
@@ -2,17 +2,28 @@ var time='480-525|535-580|600-645|655-700|860-905|915-960|980-1025|1035-1080|114
 var strs=new Array();
 strs=time.split("|")
 var range=new Array();
+//高度适应。
+window.onload=function(){
+	var a,b,c,d,$b;
+    a=window.screen.height//获取当前页面高度。
+    b=document.getElementById("ALL");//获取最大的div的高度。
+    $b=$(b);
+    c=a-150+"px";
+    $b.css("min-height",c)
+};
+//获得表格内容
 $(document).ready(function(){
 		$.ajax({
 			type:"POST",
 			url:"http://bkxjjh.natappfree.cc/seeAuthority",
 			success:function importTable_1(data){
-	                    var a,b,c,d,e,f,g,h;
+	                    var a,b,c,d,e,f,g,h,j;
                         var i,element;
                         for(i=0;i<data.length;i++){
     	                    elementParent=document.getElementById("tableTbody"); 
                             var newTr = document.createElement("tr");
-                            newTr.setAttribute("id",i)
+                            newTr.setAttribute("id",i);
+                            newTr.setAttribute("value",data[i].id);
                             newTr.innerHTML = '<td class="sumbitTime">XXXX年XX月XX日&nbsp;XX:XX:XX</td><td class="classPlace">教室地址1</td><td class="applyTime">XX月XX日&nbsp;第XX节~第XX节</td><td class="Office">真实存在的单位</td><td class="Applicant">XXX</td><td class="Reason">合乎情理没有毛病的理由</td><td class="authorityAcross_0"><img src="images/frame.png" onClick="show_element(this)"></td><td class="authorityAcross_1"><img src="images/frame.png"></td>'
                             elementParent.appendChild(newTr);
                             element=document.getElementById(i).getElementsByTagName("td");
@@ -31,6 +42,7 @@ $(document).ready(function(){
     	                    g=data[i].reason;
     	                    element[5].innerHTML=g;////,g用来与事由匹配
     	                }
+    	                
                     },
 			data:{
 				target:'getdata',
@@ -38,29 +50,45 @@ $(document).ready(function(){
 			}
 		});
 })
-//点击通过后，向后台发送请求改变数据状态。
-// $(function(){
-// 	$(".authorityAcross_0").click(function(){
-// 		Id= $('this').parent().parent().attr('id');
-// 		$.ajax({
-// 			type:"POST",
-// 			url:"http://bkxjjh.natappfree.cc/seeAuthority",
-// 			data:{
-// 				target:'getdata',
-// 				status:'0',
-// 				id:'Id',
-// 			}
-// 			success:show_element(this);
-// 			}
-// 		});
-// 	});
-// })
+//计算申请的课
+function findClass(a,b){
+	var start_time,end_time;
+	var i,k;
+	for(i=0;i<strs.length;i++){
+		range=strs[i].split("-");
+		if(parseInt(a)>=parseInt(range[0]) && parseInt(a)<=parseInt(range[1])){
+			start_time=i+1;
+		}
+		if(parseInt(b)>=parseInt(range[0]) && parseInt(b)<=parseInt(range[1])){
+			end_time=i+1;
+		}
+	};
+	if(start_time==end_time){
+		k="第"+start_time+"节";
+		return k;
+	}else{
+        k="第"+start_time+"节"+"-"+"第"+end_time+"节";
+		return k;
+	}
+}
+//点击通过后，向后台发送请求改变数据状态,并且完成一系列操作。
 function show_element(e){  
 	var a,b;
+	 var Id= $(e).parent().parent().attr('value');
+	 console.log(Id);
+		$.ajax({
+			 type:"POST",
+			 url:"http://bkxjjh.natappfree.cc/seeAuthority",
+			 data:{
+				target:'updata',
+				status:'-1',
+				id:Id,
+			 },
+			 success:(data)=>{console.log(data)}
+			})
     function first(e){
 		e.src="images/tick.png";
 		second(e);
-    	//setTimeout(function(){second(e)},1000);
     };
     function second(e){
         b=e.parentNode.parentNode;//获取tr
@@ -77,62 +105,4 @@ function show_element(e){
     e.onClick=null;
     first(e);
     
-}
-
-/*$(function(){
-	$("#Waiting").click(function(){
-		$.ajax({
-			type:"POST",
-			url:"seeAuthority",
-			//success:
-			data:{
-				target:'0',
-			}
-		});
-	});
-})
-$(function(){
-	$("#Pass").click(function(){
-		$.ajax({
-			type:"POST",
-			url:"seeAuthority",
-			//success:
-			data:{
-				target:'1',
-			}
-		});
-	});
-})
-$(function(){
-	$("#unPass").click(function(){
-		$.ajax({
-			type:"POST",
-			url:"seeAuthority",
-			//success:
-			data:{
-				target:'2',
-			}
-		});
-	});
-})*/
-//计算申请的课
-function findClass(a,b){
-	var start_time,end_time;
-	var i,k;
-	for(i=0;i<strs.length;i++){
-		range=strs[i].split("-");
-		if(a>=range[0] && a<=range[1]){
-			start_time=i+1;
-		}
-		if(b>=range[0] && b<=range[1]){
-			end_time=i+1;
-		}
-	};
-	if(start_time==end_time){
-		k="第"+start_time+"节";
-		return k;
-	}else{
-        k="第"+start_time+"节"+"-"+"第"+end_time+"节";
-		return k;
-	}
 }
