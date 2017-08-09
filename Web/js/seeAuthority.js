e2fcf1f01c3af049b84f25cf46540cd1db41db7e
changchangge <1173484831@qq.com>
2017-08-09 10:34:06 +0800

    js代码做了些优化，主要是函数的封装

--- Web/js/seeAuthority.js
+++ Web/js/seeAuthority.js
@@ -11,9 +11,77 @@ window.onload=function(){
     c=a-150+"px";
     $b.css("min-height",c)
 };
-//获得表格内容
+//首次加载获取表格内容
 $(document).ready(function(){
+    getTable();
+})
+//计算申请的课
+function findClass(a,b){
+	var start_time,end_time;
+	var i,k;
+	for(i=0;i<strs.length;i++){
+		range=strs[i].split("-");
+		if(parseInt(a)>=parseInt(range[0]) && parseInt(a)<=parseInt(range[1])){
+			start_time=i+1;
+		}
+		if(parseInt(b)>=parseInt(range[0]) && parseInt(b)<=parseInt(range[1])){
+			end_time=i+1;
+		}
+	};
+	if(start_time==end_time){
+		k="第"+start_time+"节";
+		return k;
+	}else{
+        k="第"+start_time+"节"+"-"+"第"+end_time+"节";
+		return k;
+	}
+}
+//点击通过后，向后台发送请求改变数据状态,并且完成一系列操作。
+function show_element(e,ok){  
+	var a,b,c;
+	var Id= $(e).parent().parent().attr('value');
 		$.ajax({
+			 type:"POST",
+			 url:"http://bkxjjh.natappfree.cc/seeAuthority",
+			 data:{
+				target:'updata',
+				status:ok,
+				id:Id,
+			 },
+			 // success:function(){
+			 // 	if()
+			 // }
+			})
+    function first(e){
+		e.src="images/tick.png";
+		second(e);
+    };
+    function second(e){
+        b=e.parentNode.parentNode;//获取tr
+		var c=$(b);
+		c.find('td').animate({height:"0px"},700);
+		c.find('td')
+		.wrapInner('<div style="display: block;" />')
+		.parent()
+		.find('td > div')
+		.slideUp(700, function(){
+			$(this).parent().parent().remove();
+		})
+		setTimeout(function(){
+			a=document.getElementById("tableTbody");//获取tbody标签
+            c=a.getElementsByTagName('tr');//获取所有的tr标签
+        if(c.length<3){
+    	     getTable();
+           }//点击完了重新获取
+       },710);
+    };
+    e.onClick=null;
+    first(e);
+}
+
+//获取表格内容
+function getTable(){
+	$.ajax({
 			type:"POST",
 			url:"http://bkxjjh.natappfree.cc/seeAuthority",
 			success:function importTable_1(data){
@@ -24,7 +92,7 @@ $(document).ready(function(){
                             var newTr = document.createElement("tr");
                             newTr.setAttribute("id",i);
                             newTr.setAttribute("value",data[i].id);
-                            newTr.innerHTML = '<td class="sumbitTime">XXXX年XX月XX日&nbsp;XX:XX:XX</td><td class="classPlace">教室地址1</td><td class="applyTime">XX月XX日&nbsp;第XX节~第XX节</td><td class="Office">真实存在的单位</td><td class="Applicant">XXX</td><td class="Reason">合乎情理没有毛病的理由</td><td class="authorityAcross_0"><img src="images/frame.png" onClick="show_element(this)"></td><td class="authorityAcross_1"><img src="images/frame.png"></td>'
+                            newTr.innerHTML = '<td class="sumbitTime">XXXX年XX月XX日&nbsp;XX:XX:XX</td><td class="classPlace">教室地址1</td><td class="applyTime">XX月XX日&nbsp;第XX节~第XX节</td><td class="Office">真实存在的单位</td><td class="Applicant">XXX</td><td class="Reason">合乎情理没有毛病的理由</td><td class="authorityAcross_0"><img src="images/frame.png" onClick="show_element(this,-1)"></td><td class="authorityAcross_1"><img src="images/frame.png" onClick="show_element(this,1)"></td>'
                             elementParent.appendChild(newTr);
                             element=document.getElementById(i).getElementsByTagName("td");
     	                    console.log(data[i]);
@@ -49,60 +117,4 @@ $(document).ready(function(){
 				status:'0',
 			}
 		});
-})
-//计算申请的课
-function findClass(a,b){
-	var start_time,end_time;
-	var i,k;
-	for(i=0;i<strs.length;i++){
-		range=strs[i].split("-");
-		if(parseInt(a)>=parseInt(range[0]) && parseInt(a)<=parseInt(range[1])){
-			start_time=i+1;
-		}
-		if(parseInt(b)>=parseInt(range[0]) && parseInt(b)<=parseInt(range[1])){
-			end_time=i+1;
-		}
-	};
-	if(start_time==end_time){
-		k="第"+start_time+"节";
-		return k;
-	}else{
-        k="第"+start_time+"节"+"-"+"第"+end_time+"节";
-		return k;
-	}
-}
-//点击通过后，向后台发送请求改变数据状态,并且完成一系列操作。
-function show_element(e){  
-	var a,b;
-	 var Id= $(e).parent().parent().attr('value');
-	 console.log(Id);
-		$.ajax({
-			 type:"POST",
-			 url:"http://bkxjjh.natappfree.cc/seeAuthority",
-			 data:{
-				target:'updata',
-				status:'-1',
-				id:Id,
-			 },
-			 success:(data)=>{console.log(data)}
-			})
-    function first(e){
-		e.src="images/tick.png";
-		second(e);
-    };
-    function second(e){
-        b=e.parentNode.parentNode;//获取tr
-		var c=$(b);
-		c.find('td').animate({height:"0px"},700);
-		c.find('td')
-		.wrapInner('<div style="display: block;" />')
-		.parent()
-		.find('td > div')
-		.slideUp(700, function(){
-			$(this).parent().parent().remove();
-		})
-    };
-    e.onClick=null;
-    first(e);
-    
 }
